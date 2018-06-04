c42c2e2ae4f981f3aafe4d56e0da28d0ca473298
valestro <39577274+valestro@users.noreply.github.com>
2018-06-03 23:42:19 -0500

    Create 2_SlicingUpStrings.py

---
+++ 2_SlicingUpStrings.py
@@ -0,0 +1,9 @@
+user = 'ABCDEFGH'
+print(user[0])                          # A
+print(user[1])                          # B
+print(user[2:6])                        # CDEF
+print(user[:3])                         # ABC
+print(user[2:])                         # CDEFGH
+print(user[:])                          # ABCDEFGH
+print(len(user))                        # 8
+print(user[-3], user[-5], user[1])      # F D B. Extra. Append from right to left with -. Also, multiple print statements in one line seperated by commas.
